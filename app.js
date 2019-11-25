705101f4a76839a2aba27ab47095089c15f76594
Lorenzo Lando <lorenzo.lando85@gmail.com>
2019-11-25 20:42:05 +0000

    new comments

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require("express"),
 	  locus = require("locus"), //per fare debugging
 	  app = express(), //creo una istanza di esxpress
 	  viewEngine = require("view-engine"), //per eliminare ejs estensioni
-	  bodyParser = require("body-parser"), //faccio il require del bodyparser
+	  bodyParser = require("body-parser"), //faccio il require del bodyparser serve a rendere il body un oggetto javascript
 	  mongoose = require("mongoose"), //mongoose to add js to databases
 	  Campground = require("./models/campground"), //sto importando mongoose.model("Campgroud", campgroundSchema);
 	  seedDB = 	require("./seeds");	
@@ -58,36 +58,38 @@ app.get("/", (req, res) => {
 			if(err){
 				console.log(err);
 			} else {
-				console.log(allCampgrounds);
-				res.render("index", { campgrounds: allCampgrounds}); //3
+				//console.log(allCampgrounds);
+				res.render("campgrounds/index", { campgrounds: allCampgrounds}); //3
 			}
 		});
 	});
 
 //CREATE -  RESTFUL ROUTING STRUCTURE e` il form per aggiungere un item
 //CREATE e lo step di aggiunta al database
-//1 create a new campground e lo inseriscilo nel database
+//1 setto i parametri che mi arrivano dalla post request contenuti nell`oggetto req.body
+//1.2 create a new campground e lo inseriscilo nel database
 //2 ci sono due /campground, lo manda in quello corretto perche` get ha la priorita`
 //2 dopo la post request re-indirizzo nella pagina campground	
 
 	app.post("/campgrounds", (req, res) => {
+		
   		var name = req.body.name;
    		var image = req.body.image;
 		var description = req.body.description;
 		var newCampground = {name: name, image: image, description: description};
-				Campground.create(newCampground, (err, added) => {
+				Campground.create(newCampground, (err, added) => { //1.2
 					if(err) {
 						console.log(err)
 					} else {
 						
-						res.redirect("/campgrounds");	//2
+						res.redirect("campgrounds/campgrounds");	//2
 					}
 			});
 	});
 
 //NEW -  RESTFUL ROUTING STRUCTURE mostra la form attraverso la quale possimao creare un nuovo item
 app.get("/campgrounds/new", (req, res) => {
-  res.render("new");
+  res.render("campgrounds/new");
 });
 
 //SHOW -  RESTFUL ROUTING STRUCTURE mostra le info a proposito di uno specifico item 
@@ -98,11 +100,40 @@ app.get("/campgrounds/:id", (req, res) => {
 		if(err) {
 			console.log(err);
 		} else {
-		    res.render("show", {campground: foundCampground}); //2
+		    res.render("campgrounds/show", {campground: foundCampground}); //2
 		}
 	});
 	
 }); 
+
+
+//////////////////////////////////////////////////////////////////////////////////////////
+//ROUTES PER I COMMENTI
+//////////////////////////////////////////////////////////////////////////////////////////
+
+//NEW--RESTFUL ROUTING STRUCTURE serve a mostrare il form attraverso il quale possiamo creare un nuovo commento
+//1-quando avviene una http request 
+//2- faccio il render di un file new.ejs
+//3 trovo i camground by id dallo url
+//4 il campground ottenuto lo invio come variabile al template che renderizzo
+app.get("/campgrounds/:id/comments/new", (req, res) => { //1
+	Campground.findById(req.params.id, (err, campground)=> {
+		if(err) {
+			console.log(err);
+		} else {
+			
+			res.render("comments/new", {campground: campground}); //4
+		}
+	})
+	
+}); 
+
+
+
+
+
+
+
 
 //Mi connetto al server NB: questa riga puo` andare in qualunque posizione del file il metodo viene richiamato ogni volta 
 //che viene fatta una richiesta in questo port.
